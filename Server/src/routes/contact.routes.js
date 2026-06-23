c8d2138474320e3d4f6f2777ee9e879d99b41971
Pankti0985 <shahpankti985@gmail.com>
2026-06-23 10:07:45 +0530

    move server folder in backend folder

--- Server/src/routes/contact.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { submitContact } = require("../controllers/contact.controller");
-
-router.post("/", submitContact);
-
-module.exports = router;
